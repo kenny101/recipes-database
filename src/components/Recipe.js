7162b4221f077ce9a50737fdcdb776ae7fc3b4df
Kenny <kennychan42101@gmail.com>
2021-06-25 04:09:39 -0700

    added href to cards

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,10 @@
-import { Box, Image, Badge, Stack, Text, Divider } from "@chakra-ui/react";
+import { Box, Image, Badge, Stack, Text, Link } from "@chakra-ui/react";
 
 
 const Recipe = ({ recipe, onFilter }) => {
     return (
-            <Box w='400px' rounded='10px' overflow='hidden'boxShadow='2xl' bg='gray.200' margin='10px 5px'>
+        <Link href={recipe.Source} isExternal>
+            <Box w='400px' rounded='10px' overflow='hidden' boxShadow='2xl' bg='gray.200' margin='10px 5px' cursor="pointer">
                 <Image src={recipe.Image} alt='card view link to a recipe' />
                 <Box p={2}>
                     <Text as='b'>{recipe.Name}</Text>
@@ -19,6 +20,7 @@ const Recipe = ({ recipe, onFilter }) => {
                     </Box>
                 </Box>
             </Box>
+        </Link>
     );
 };
 
